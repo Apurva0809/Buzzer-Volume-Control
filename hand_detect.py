7ac20e572b6db6c91b1c0192c744778779d9d739
Apurva0809 <67169527+Apurva0809@users.noreply.github.com>
2021-07-23 20:23:05 +0530

    Add files via upload

---
+++ hand_detect.py
@@ -0,0 +1,58 @@
+import cv2
+import mediapipe as mp
+
+class handDetector():
+    def __init__(self, mode = False, maxHands = 2, detectionCon = 0.5, trackCon = 0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,
+                                        self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, img, draw = True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms,
+                                               self.mpHands.HAND_CONNECTIONS)
+        return img
+
+    def findPosition(self, img, handNum=0, draw=True):
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNum]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 15, (235, 0, 180), -1)
+        return lmList
+
+def main():
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
+    while True:
+        success, frame = cap.read()
+        frame = detector.findHands(frame)
+        lmList = detector.findPosition(frame)
+        if len(lmList) != 0:
+            print(lmList[4], lmList[8])
+
+        cv2.imshow("Frame", frame)
+        cv2.waitKey(1)
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
+    

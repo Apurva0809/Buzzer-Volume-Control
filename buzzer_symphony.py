1bba0fbbb4b1e991bb8a11d3d50c513fc867712c
Apurva0809 <67169527+Apurva0809@users.noreply.github.com>
2021-07-27 14:42:28 +0530

    Update buzzer_symphony.py

--- buzzer_symphony.py
+++ buzzer_symphony.py
@@ -23,26 +23,24 @@ while True:
         x2, y2 = lmList[8][1], lmList[8][2]
         cx, cy = ((x1+x2)//2), ((y1+y2)//2)
         
-        cv2.circle(frame, (x1, y1), 10, (255, 0, 255), cv2.FILLED)
-        cv2.circle(frame, (x2, y2), 10, (255, 0, 255), cv2.FILLED)
-        cv2.circle(frame, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+        cv2.circle(frame, (x1, y1), 10, (255, 0, 255), -1)
+        cv2.circle(frame, (x2, y2), 10, (255, 0, 255), -1)
+        cv2.circle(frame, (cx, cy), 10, (255, 0, 255), -1)
         cv2.line(frame, (x1, y1), (x2, y2), (219, 112, 147), 2)
         
         length = math.hypot(x2-x1, y2-y1)
         volumeBar = np.interp(length, [20,150], [400, 150])
-        #print(length)
+        volume = np.interp(length, [20,150], [0, 255])
         
         if length < 20:
-            cv2.circle(frame, (cx, cy), 10, (255, 255, 0), cv2.FILLED)
+            cv2.circle(frame, (cx, cy), 10, (255, 255, 0), -1)
             
         cv2.rectangle(frame, (50, 150), (85, 400), (127, 255, 0), 3)
         cv2.rectangle(frame, (50, int(volumeBar)), (85, 400), (154, 250, 0), -1)
         
         if mode == True:
-            mybolt.analogWrite('0', int(length))
-            print(int(length))
-            #time.sleep(1)
-            mybolt.digitalWrite('0','LOW')
+            mybolt.analogWrite('0', int(volume))
+            #print(int(length), int(volume))
             mode = False
             
     cv2.imshow("Frame", frame)
@@ -51,6 +49,7 @@ while True:
         mode = not mode
     elif k == 27:
         break
+mybolt.digitalWrite('0','LOW')
 cap.release() 
 cv2.destroyAllWindows()
 
